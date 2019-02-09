503c8bfe1f747f2eaaf27b2236193adc1d34a226
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 20:51:02 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,31 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+ cart.uniq.each do |unique_item|
+   count = 0 
+   cart.each do |item|
+     if unique_item == item
+       count +=1
+     end
+   end
+   unique_item.each do |name, info|
+     new_cart[name] = info
+     new_cart[name][:count] = count
+   end
+  end
+ new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  cart.each do |name, info|
+    if cart[name] == coupons[:item]
+      binding.pry
+    end
+    end
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
